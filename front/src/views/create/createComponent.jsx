bead306c8531e8a92027584624275a682ccbccfa
rubencorba <rubencorba@gmail.com>
2024-10-02 21:34:35 -0300

    formulario con input de archivo

--- front/src/views/create/createComponent.jsx
+++ front/src/views/create/createComponent.jsx
@@ -1,84 +1,154 @@
+import { useState } from 'react';
 import './createStyles.css';
 
 function Create() {
+
+  
+    const [selectedFile, setSelectedFile] = useState(null);
+  
+    const handleFileChange = (event) => {
+      setSelectedFile(event.target.files[0]);
+      console.log(event.target.files[0]); // Puedes ver el archivo en la consola
+    };
+
+    const handleDrop = (event) => {
+      event.preventDefault(); // Evita que el navegador abra el archivo
+      const file = event.dataTransfer.files[0];
+      setSelectedFile(file);
+      console.log(file); // Ver el archivo en consola
+    };
+  
+    const handleDragOver = (event) => {
+      event.preventDefault(); // Evita el comportamiento por defecto
+    };
+
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full">
+      <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full my-24">
     <form>
       <div class="space-y-12">
-        <div class="border-b border-gray-900/10 pb-12">
-        <div class="border-b border-gray-900/10 pb-12">
-      <h2 class="text-base font-semibold leading-7 text-gray-900">Personal Information</h2>
-      <p class="mt-1 text-sm leading-6 text-gray-600">Use a permanent address where you can receive mail.</p>
+        {/* <div class="border-b border-gray-900/10 pb-12"> */}
+        {/* <div class="border-b border-gray-900/10 pb-12"> */}
+      <h2 class="text-base font-semibold leading-7 text-gray-900">Información personal para el Trámite</h2>
+      {/* <p class="mt-1 text-sm leading-6 text-gray-600">Use a permanent address where you can receive mail.</p> */}
 
       <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-        <div class="sm:col-span-3">
-          <label for="first-name" class="block text-sm font-medium leading-6 text-gray-900">First name</label>
+        <div class="sm:col-span-4">
+          <label for="first-name" class="block text-sm font-medium leading-6 text-gray-900">Apellido y Nombre</label>
           <div class="mt-2">
             <input type="text" name="first-name" id="first-name" autocomplete="given-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
           </div>
         </div>
 
-        <div class="sm:col-span-3">
+        {/* <div class="sm:col-span-3">
           <label for="last-name" class="block text-sm font-medium leading-6 text-gray-900">Last name</label>
           <div class="mt-2">
             <input type="text" name="last-name" id="last-name" autocomplete="family-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
           </div>
-        </div>
+        </div> */}
+
+          <div class="sm:col-span-4">
+            <label htmlFor="fecha-nacimiento" className="block text-sm font-medium text-gray-700 leading-6">
+              Fecha de Nacimiento
+            </label>
+            <div className="mt-2">
+            <input
+              id="fecha-nacimiento"
+              name="fecha-nacimiento"
+              type="date"
+              required
+              className=" block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+            </div>
+          </div>
 
         <div class="sm:col-span-4">
-          <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+          <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Numero de DNI</label>
           <div class="mt-2">
             <input id="email" name="email" type="email" autocomplete="email" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
           </div>
         </div>
 
         <div class="sm:col-span-3">
-          <label for="country" class="block text-sm font-medium leading-6 text-gray-900">Country</label>
+          <label for="country" class="block text-sm font-medium leading-6 text-gray-900">Sexo</label>
           <div class="mt-2">
             <select id="country" name="country" autocomplete="country-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
-              <option>United States</option>
-              <option>Canada</option>
-              <option>Mexico</option>
+              <option>Masculino</option>
+              <option>Femenino</option>
+              <option>Otro</option>
             </select>
           </div>
         </div>
 
-        <div class="col-span-full">
+        {/* <div class="col-span-full">
           <label for="street-address" class="block text-sm font-medium leading-6 text-gray-900">Street address</label>
           <div class="mt-2">
             <input type="text" name="street-address" id="street-address" autocomplete="street-address" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
           </div>
-        </div>
+        </div> */}
 
         <div class="sm:col-span-2 sm:col-start-1">
-          <label for="city" class="block text-sm font-medium leading-6 text-gray-900">City</label>
+          <label for="city" class="block text-sm font-medium leading-6 text-gray-900">Calle</label>
           <div class="mt-2">
             <input type="text" name="city" id="city" autocomplete="address-level2" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
           </div>
         </div>
 
         <div class="sm:col-span-2">
-          <label for="region" class="block text-sm font-medium leading-6 text-gray-900">State / Province</label>
+          <label for="region" class="block text-sm font-medium leading-6 text-gray-900">Numero</label>
           <div class="mt-2">
             <input type="text" name="region" id="region" autocomplete="address-level1" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
           </div>
         </div>
 
         <div class="sm:col-span-2">
-          <label for="postal-code" class="block text-sm font-medium leading-6 text-gray-900">ZIP / Postal code</label>
+          <label for="postal-code" class="block text-sm font-medium leading-6 text-gray-900">Dpto-Piso</label>
           <div class="mt-2">
             <input type="text" name="postal-code" id="postal-code" autocomplete="postal-code" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
           </div>
         </div>
+
+
+
+      <div class="col-span-full">
+          <label for="cover-photo" class="block text-sm font-medium leading-6 text-gray-900">Foto DNI frontal</label>
+          <div
+            class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10"
+           
+            onDrop={handleDrop}
+            onDragOver={handleDragOver}>
+            <div class="text-center">
+              <svg class="mx-auto h-12 w-12 text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+                <path fill-rule="evenodd" d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z" clip-rule="evenodd" />
+              </svg>
+              <div class="mt-4 flex text-sm leading-6 text-gray-600">
+                <label for="file-upload" class="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500">
+                  <span>Subir archivo</span>
+                  <input
+                   id="file-upload" 
+                   name="file-upload" 
+                   type="file" 
+                   class="sr-only"
+                   
+                   onChange={handleFileChange}/>
+                </label>
+                <p class="pl-1">o arrastre la foto hasta aquí</p>
+              </div>
+              <p class="text-xs leading-5 text-gray-600">PNG, JPG, GIF hasta 10MB</p>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+
+
       <div class="mt-6 flex items-center justify-end gap-x-6">
-        <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
-        <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+        <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancelar</button>
+        <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Enviar</button>
       </div>
     </div>
-    </div>
+    {/* </div> */}
+    {/* </div> */}
     </form>
     </div>
     </div>
